65d631a83347e00453352039713ffa09fae0ac66
saqib <saqibb07@github.com>
2023-01-10 08:53:55 +0500

    subtraction functionality implemented

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #main file created
+
+#func to subtract two numbers 
+def sub(x,y):
+  result=x-y
+  print(x,"-",y,"=",result)
+n1=int(input("enter the first number:"))  
+n2=int(input("enter the second number:"))  
+#func call
+sub(n1,n2)

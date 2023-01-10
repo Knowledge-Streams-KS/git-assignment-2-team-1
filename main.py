4708a95fee222ece20e160ce6a4db41b16d720c3
saqib <saqibb07@github.com>
2023-01-10 08:55:47 +0500

    multiplication functionality implemented

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #main file created
+
+#func to multiply two numbers 
+def mul(x,y):
+  result=x*y
+  print(x,"*",y,"=",result)
+n1=int(input("enter the first number:"))  
+n2=int(input("enter the second number:"))  
+#func call
+mul(n1,n2)

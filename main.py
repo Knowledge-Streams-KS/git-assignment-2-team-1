eb87456590ea442b474d5237d2aa6ca0d46d2ffd
saqib <saqibb07@github.com>
2023-01-10 08:58:55 +0500

    modulus functionality implemented

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #main file created
+
+#func to modulus two numbers 
+def mod(x,y):
+  result=x%y
+  print(x,"%",y,"=",result)
+n1=int(input("enter the first number:"))  
+n2=int(input("enter the second number:"))  
+#func call
+mod(n1,n2)

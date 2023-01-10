6bcaf3148e892fb153e7561451e1b1bd18c95747
saqib <saqibb07@github.com>
2023-01-10 08:28:12 +0500

    addition functionality implemented

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #main file created
+
+#func to add two numbers 
+def add(x,y):
+  result=x+y
+  print(x,"+",y,"=",result)
+n1=int(input("enter the first number:"))  
+n2=int(input("enter the second number:"))  
+#func call
+add(2,3)

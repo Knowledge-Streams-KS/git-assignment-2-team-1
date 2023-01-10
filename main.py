4ca7eecb40cae9e03710432dad2790ad20946df9
saqib <saqibb07@github.com>
2023-01-10 09:00:34 +0500

    square-root functionality implemented

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 #main file created
+
+#square root of a number
+import math
+num = float(input(" Enter a number: "))
+sqRoot = math.pow(num, 0.5)
+print("The square root of a given number {0} = {1}".format(num, sqRoot))
